eb0b5bbe1b59fd565a28dcd5e17ef62a0ce67a92
Jinskiss <135427721+Jinskiss@users.noreply.github.com>
2024-04-14 05:11:03 +0000

    “update”

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -3,10 +3,8 @@
 // Make the code print a greeting to the world.
 //
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// hint.r
 
 fn main() {
-    println!("Hello {}!");
+    println!("Hello!");
 }
