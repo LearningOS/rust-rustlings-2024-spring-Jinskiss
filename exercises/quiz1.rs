f6a183f329fbca0f72e140398e8137bba4bf1079
Jinskiss <135427721+Jinskiss@users.noreply.github.com>
2024-04-14 09:00:36 +0000

    “update”

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,8 +16,9 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(x: i32) -> i32 {
+    x * 2
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
